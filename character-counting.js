81effe3cf90cb39fbf83cb9c8cabd6f9849d39bf
landen danyluk <landendanyluk@gmail.com>
2018-09-25 23:32:08 +0000

    Don't be fancy

--- character-counting.js
+++ character-counting.js
@@ -1,5 +1,6 @@
 function countCharacters(string) {
-  let stringNoSpaces = string.split(' ').join(''), result = {};
+  let stringNoSpaces = string.split(' ').join('');
+  let result = {};
   for (let character of stringNoSpaces) {
     if (!result[character]) { result[character] = 1; break; }
     result[character]++;
