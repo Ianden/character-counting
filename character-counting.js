1011869e3b688b09cc22ea661d083f51f06bd976
landen danyluk <landendanyluk@gmail.com>
2018-09-25 23:31:12 +0000

    Initial commit

---
+++ character-counting.js
@@ -0,0 +1,10 @@
+function countCharacters(string) {
+  let stringNoSpaces = string.split(' ').join(''), result = {};
+  for (let character of stringNoSpaces) {
+    if (!result[character]) { result[character] = 1; break; }
+    result[character]++;
+  }
+  return result;
+}
+
+console.log(countCharacters("lighthouse in the house"));
